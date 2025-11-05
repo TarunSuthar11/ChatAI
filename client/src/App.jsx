ea1ed957055f4fd59febc8a7614c7d3006ddc07c
Tarun <tarunsuthar1104>
2025-11-05 11:48:28 +0530

    created all the pages required for the project

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import {route} from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, consectetur aliquam quae soluta consequatur illum dolorum! Deserunt, voluptas magni accusamus beatae id, quos, ullam labore quia consequatur odio autem debitis.</p>
+    </>
+  )
+}
+
+export default App
